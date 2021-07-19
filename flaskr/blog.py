f35c349d48e7db121637256e0792742cd76f9a27
K K <1031599+biofool@users.noreply.github.com>
2021-07-19 12:54:41 -0700

    latitude, longitude added

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -18,20 +18,20 @@ def index():
     """Show all the posts, most recent first."""
     db = get_db()
     posts = db.execute(
-        "SELECT p.id, title, body, created, author_id, username"
+        "SELECT p.id, title, body, latitude, longitude, created, author_id, username"
         " FROM post p JOIN user u ON p.author_id = u.id"
         " ORDER BY created DESC"
     ).fetchall()
     return render_template("blog/index.html", posts=posts)
 
 
-def get_post(id, check_author=True):
+def get_post(db, check_author=True):
     """Get a post and its author by id.
 
     Checks that the id exists and optionally that the current user is
     the author.
 
-    :param id: id of post to get
+    :param db: id of post to get
     :param check_author: require the current user to be the author
     :return: the post with author information
     :raise 404: if a post with the given id doesn't exist
@@ -40,16 +40,16 @@ def get_post(id, check_author=True):
     post = (
         get_db()
         .execute(
-            "SELECT p.id, title, body, created, author_id, username"
+            "SELECT p.id, title, body, created, author_id, latitude, longitude, username"
             " FROM post p JOIN user u ON p.author_id = u.id"
             " WHERE p.id = ?",
-            (id,),
+            (db,),
         )
         .fetchone()
     )
 
     if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
+        abort(404, f"Post id {db} doesn't exist.")
 
     if check_author and post["author_id"] != g.user["id"]:
         abort(403)
@@ -64,7 +64,8 @@ def create():
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
-        body = request.form["latitude"]
+        latitude = request.form["latitude"]
+        longitude = request.form["longitude"]
 
         error = None
 
@@ -76,8 +77,8 @@ def create():
         else:
             db = get_db()
             db.execute(
-                "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
-                (title, body, g.user["id"]),
+                "INSERT INTO post (title, body, latitude, longitude, author_id) VALUES (?, ?, ?, ?, ?)",
+                (title, body, latitude, longitude, g.user["id"]),
             )
             db.commit()
             return redirect(url_for("blog.index"))
@@ -85,15 +86,18 @@ def create():
     return render_template("blog/create.html")
 
 
-@bp.route("/<int:id>/update", methods=("GET", "POST"))
+@bp.route("/<dbid>/update", methods=("GET", "POST"))
 @login_required
-def update(id):
+def update(dbid):
     """Update a post if the current user is the author."""
-    post = get_post(id)
+    post = get_post(dbid)
 
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
+        latitude = request.form["latitude"]
+        longitude = request.form["longitude"]
+
         error = None
 
         if not title:
@@ -104,7 +108,7 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
+                "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, dbid, latitude, longitude)
             )
             db.commit()
             return redirect(url_for("blog.index"))
@@ -112,16 +116,16 @@ def update(id):
     return render_template("blog/update.html", post=post)
 
 
-@bp.route("/<int:id>/delete", methods=("POST",))
+@bp.route("/<record_id>/delete", methods=("POST",))
 @login_required
-def delete(id):
+def delete(record_id):
     """Delete a post.
 
     Ensures that the post exists and that the logged in user is the
     author of the post.
     """
-    get_post(id)
+    get_post(record_id)
     db = get_db()
-    db.execute("DELETE FROM post WHERE id = ?", (id,))
+    db.execute("DELETE FROM post WHERE id = ?", (record_id,))
     db.commit()
     return redirect(url_for("blog.index"))
