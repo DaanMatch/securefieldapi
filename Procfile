5b2ae17d69bdc56b5b489356273b21d556a743f5
Kenneth Kron <1031599+biofool@users.noreply.github.com>
2021-07-22 15:59:26 -0700

    herokuize.py

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
-release: flask init-db
+release: export FLASK_APP=flasrk ;flask init-db
 web: gunicorn securefieldapi:flaskr
 
--- herokuize.py
+++ herokuize.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import re
 import subprocess
 import requests
@@ -50,5 +46,3 @@ def herokuize(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     herokuize('create.log')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
