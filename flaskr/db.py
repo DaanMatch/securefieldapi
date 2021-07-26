f1922cfdfd5536fa2f16c3d267cb76abd743e273
KK <1031599+biofool@users.noreply.github.com>
2021-07-26 14:25:57 -0700

    Check for path of sqllite

--- flaskr/db.py
+++ flaskr/db.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import os
 import click
 from flask import current_app
 from flask import g
@@ -11,8 +11,10 @@ def get_db():
     is unique for each request and will be reused if this is called
     again.
     """
-    if "db" not in g:
+    if (not os.path('instance/flaskr.sqlite')):
         init_db_command()
+
+    if "db" not in g:
         g.db = sqlite3.connect(
             current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
         )
