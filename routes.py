c670cdc302a72b939e6a52c40492bb1d86fbc883
mitchell2001wong <mitchell2001wong@gmail.com>
2021-10-15 08:49:55 -0700

    Progress on members seeing their field_data

--- routes.py
+++ routes.py
@@ -4,8 +4,10 @@ from resource_managers import *
 
 # Format: api.route(<Resource manager>, <endpoint name>, <url_1>, <url_2>, ...)
 
+# TODO: member/id/field_data url needs to only get data from member.id
 # Provides GET and POST to /field_data
-api.route(FieldDataMany, 'field_data_many', '/member/<int:id>/field_data')
+api.route(FieldDataMany, 'field_data_many', 
+    '/field_data', '/member/<int:id>/field_data')
 
 # Provides GET, PATCH, and DELETE to /field_data/<int:id>
 api.route(FieldDataOne, 'field_data_one', '/field_data/<int:id>')
@@ -14,13 +16,9 @@ api.route(FieldDataOne, 'field_data_one', '/field_data/<int:id>')
 api.route(MemberOne, 'member_one', '/member/<int:id>')
 
 
-
-# # ????
-# # Provides GET, POST to /member/<int:id>/field_data
+# Might delete this, unsure
+# Provides GET, POST to /member/<int:id>/field_data
 # api.route(MemberFieldData, 'member_field_data', '/member/<int:id>/field_data')
-
-
-
 
 
 # FOR TESTING ONLY, TODO: DELETE THIS LATER
--- schemas/member_schema.py
+++ schemas/member_schema.py
@@ -45,8 +45,8 @@ class MemberSchema(Schema):
 
     # args similar to meta class attributes
     field_data = Relationship(
-        self_view = 'member_field_data',
-        self_view_kwargs = {'id': '<id>'},
+        # self_view = 'member_field_data',
+        # self_view_kwargs = {'id': '<id>'},
         related_view = 'field_data_one',
         related_view_kwargs = {'id': '<id>'},
         many = True, 
