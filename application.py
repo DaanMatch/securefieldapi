9086258b180102df387706244bbe6073e05f9247
mitchell2001wong <mitchell2001wong@gmail.com>
2021-11-09 11:52:37 -0800

    Added rate limit for login

--- application.py
+++ application.py
@@ -1,33 +1,26 @@
-from flask import Flask
 
-from config import SECRET_KEY
+
+from app import app # Import the Flask() app to prevent circular imports
 from db import db # SQLAlchemy object imported to prevent circular imports
 from api import api # Api object imported from flask_rest_jsonapi to prevent circular imports
 from models import *
 from schemas import *
 from resource_managers import *
 from routes import *
+from limiter import limiter
 
 
-# Create a new Flask application
-app = Flask(__name__)
-
-
-# Set up SQLAlchemy
-app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Daanmatch.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app) # delayed initialization
-
 
 # Create the tables
 with app.app_context():
     db.create_all()
 
-
 api.init_app(app) # delayed initialization
 
+# login route, move me later
 from auth.login import login_member
+@limiter.limit("5/10minutes")
 @app.route('/login', methods=['GET', 'POST']) 
 def login():
     return login_member()
